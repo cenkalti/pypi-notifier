409b845d46fd20c3a14333d7931e516df8efef90
Cenk Alti <cenkalti@gmail.com>
2013-06-05 00:11:57 +0300

    add test

---
+++ test.py
@@ -0,0 +1,21 @@
+import unittest
+
+import pypi_notifier
+from pypi_notifier.config import TestingConfig
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = pypi_notifier.create_app(TestingConfig)
+        self.client = self.app.test_client()
+        with self.app.app_context():
+            pypi_notifier.db.create_all()
+
+    def test_index(self):
+        rv = self.client.get('/')
+        assert 'Login' in rv.data
+
+
+if __name__ == '__main__':
+    unittest.main()
