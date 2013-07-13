65c635016a708efcb9fa0e7cc4362cff9bc303e0
Cenk Alti <cenkalti@gmail.com>
2013-07-13 20:10:42 +0300

    add log messages to update_packages()

--- pypi_notifier/models/package.py
+++ pypi_notifier/models/package.py
@@ -1,8 +1,12 @@
+import logging
 import xmlrpclib
 from datetime import datetime
 from sqlalchemy.ext.associationproxy import association_proxy
 from pypi_notifier import db, cache
 from pypi_notifier.models.mixin import ModelMixin
+
+
+logger = logging.getLogger(__name__)
 
 
 class Package(db.Model, ModelMixin):
@@ -42,7 +46,9 @@ class Package(db.Model, ModelMixin):
         return self.get_all_names()[self.name.lower()]
 
     def find_latest_version(self):
-        return self.pypi.package_releases(self.original_name)[0]
+        version = self.pypi.package_releases(self.original_name)[0]
+        logger.info("Latest version of %s is %s", self.original_name, version)
+        return version
 
     def update_from_pypi(self):
         """
