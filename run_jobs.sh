cd2d555afc0d9a826f327ce4d1c9b579f732995c
Cenk Alti <cenkalti@gmail.com>
2013-07-17 14:14:19 +0300

    update comment

--- run_jobs.sh
+++ run_jobs.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR}
 
-./manage.py update_repos     # Fetches requirements
+./manage.py update_repos     # Fetches requirements from GitHub
 ./manage.py update_packages  # Checks latest versions from PyPI
 ./manage.py send_emails      # Notifies users about updates
