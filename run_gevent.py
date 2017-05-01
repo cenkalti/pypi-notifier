6b6761cc2bbccfded1848493eed1b345c6298cd6
Cenk Alti <cenkalti@gmail.com>
2017-05-01 15:15:26 +0300

    fix runserver script

--- run_gevent.py
+++ run_gevent.py
@@ -3,7 +3,7 @@ from gevent.monkey import patch_all; patch_all()  # noqa
 from gevent.pywsgi import WSGIServer
 import os
 import sys
-import pypi_notifier
-app = pypi_notifier.create_app(sys.argv[1])
+from pypi_notifier.app import create_app
+app = create_app(sys.argv[1])
 server = WSGIServer(('0.0.0.0', int(os.environ.get("PORT", 5000))), app)
 server.serve_forever()
