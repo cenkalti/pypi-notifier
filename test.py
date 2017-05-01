83eaebd8876abae5869493230b235bbefedc3243
Cenk Alti <cenkalti@gmail.com>
2017-05-01 13:37:28 +0300

    fix test

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ class PyPINotifierTestCase(unittest.TestCase):
 
     def test_index(self):
         rv = self.client.get('/')
-        assert 'Login' in rv.data
+        assert 'Login' in rv.data.decode()
 
     def test_login(self):
         rv = self.client.get('/login')
