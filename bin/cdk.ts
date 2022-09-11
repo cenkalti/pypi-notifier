f3e62e7f71fc541244f9d8c959329c6f077a8ac1
Cenk Alti <cenkalti@gmail.com>
2022-09-11 14:49:29 -0400

    add eslint config

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import * as cdk from 'aws-cdk-lib'
+import { CdkStack } from '../lib/cdk-stack'
 
-const app = new cdk.App();
+const app = new cdk.App()
 new CdkStack(app, 'CdkStack', {
 
   env: { account: '821523470585', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+})
